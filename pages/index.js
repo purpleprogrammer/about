4529aee14b2e9645dc1b551a0ce279a6c60b5911
purple <work.demonter@mail.ru>
2026-01-08 19:32:18 +0800

    add main page with hello world

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="gradient-bg min-h-screen flex items-center justify-center text-white">
+      <h1 className="text-4xl font-bold">hello world</h1>
+    </div>
+  );
+}
